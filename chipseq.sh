51bf539ba804dda2140fb74870ea4c6fafc6bcf8
Gina Turco <gturco88@gmail.com>
2013-07-31 15:31:25 -0700

    uptodate running of pipeline

--- chipseq.sh
+++ chipseq.sh
@@ -1,49 +1,54 @@
-#Geo: http://www.nature.com/ng/journal/v43/n7/full/ng.854.html
-#The ChIP-Seq dataset and ATH1 expression array dataset are deposited in Gene Expression Omnibus (GEO) under the accession code GSE25447.
-#wget http://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE25447&format=file
-#tar -xvf in
-#1-kb window size used in the ChIPDiff program
+data_path=
+##need full data path
+##################################################
+############## Convert sra to fastq ##############
+##################################################
 
-### Get Data
+fastq-dump ${data_path}/${WT}.sra
+fastq-dump ${data_path}/${KO}.sra
 
-#Get WT Data
-wget ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra/SRX%2FSRX031%2FSRX031221/SRR072991/SRR072991.sra
-fastq-dump /home/gturco/data/find_maize/chip/SRR072991.sra
-mv SRR072991.fastq WT.fastq
+#################################################
+############### Quality Control #################
+#################################################
 
-#Get KO Data
-wget ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra/SRX%2FSRX031%2FSRX031222/SRR072992/SRR072992.sra
-fastq-dump /home/gturco/data/find_maize/chip/SRR072992.sra
-mv SRR072992.sra KO.fastq
-
-#### QC
-#Data Before
+#Before
 #TODO find out why can only do from this dir
-./FastQC/fastqc KO.fastq
-./FastQC/fastqc WT.fastq
+./FastQC/fastqc ${WT_path}.fastq
+./FastQC/fastqc ${KO_path}.fastq
 
-#trim and QC read
-trimReads -q 20 -m 20 -f adapters.fasta KO.fastq
-trimReads -q 20 -m 20 -f adapters.fasta WT.fastq
+#Trim and QC
+trimReads -q 20 -m 20 -f ${data_path}/adapters.fasta ${WT_path}.fastq
+trimReads -q 20 -m 20 -f ${data_path}/adapters.fasta ${KO_path}.fastq
 
-#Data After
-./FastQC/fastqc KO.trimmed.fastq
-./FastQC/fastqc WT.trimmed.fastq
+#After
+./FastQC/fastqc ${WT_path}.trimmed.fastq
+./FastQC/fastqc ${KO_path}.trimmed.fastq
+
+#################################################
+################### BWA #########################
+#################################################
+
+python aln_bwa.py -n ${WT_path}.trimmed -i ${data_path}/${index}
+python aln_bwa.py -n ${KO_path}.trimmed -i ${data_path}/${index}
+
+#TODO add code for removing dups
+#################################################
+################### SICER #######################
+#################################################
+
+mkdir ${data_path}/SICER_res/
+python determine_gapsize.py --ko ${data_path}/sorted-${KO}.sam.bed --wt ${data_path}/sorted-${WT}.sam.bed -w 200 -o ${data_path}/SICER_res
+
+#run graph_gap_size.R 
+#TODO make a wrapper for this
+
+#run with best parmas
+best_gap=800
+sh ~/Downloads/SICER_V1.1/SICER/SICER-df-rb.sh ${data_path}/sorted-${KO}.sam.bed ${data_path}/sorted-${WT}_head.sam.bed 200 ${best_gap} 0.1 0.001 > $data_path/SICER_res/
+
+#################################################
+############## Get enrichment ###################
+#################################################
 
 
 
-
-#### BWA
-python aln_bwa.py -n KO -i genome
-python aln_bwa.py -n WT -i genome
-
-#TODO add code for removing dups!
-#sort and convert sam to bed file
-sam2bed < KO.sam > sorted-KO.sam.bed
-sam2bed < WT.sam > sorted-WT.sam.bed
-
-vim sorted-KO_head.sam.bed
-%s/^/chr
-
-#Run SICER
-sh ~/Downloads/SICER_V1.1/SICER/SICER-df-rb.sh sorted-KO_head.sam.bed sorted-WT_head.sam.bed 10000 20000 0.001 0.001 > sicer_out

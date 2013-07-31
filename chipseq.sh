02d712e3d48c437da6cff6833cbb1645880be85e
Gina Turco <gturco88@gmail.com>
2013-07-31 15:35:41 -0700

    add vars

--- chipseq.sh
+++ chipseq.sh
@@ -1,11 +1,20 @@
-data_path=
-##need full data path
+data_path= #change to some path where your data is stored
+           ##need full data path
+
+WT= #header name of WT files
+KO= #header name of KO files
+
+
 ##################################################
 ############## Convert sra to fastq ##############
 ##################################################
 
 fastq-dump ${data_path}/${WT}.sra
 fastq-dump ${data_path}/${KO}.sra
+
+WT_path=${data_path}/${WT}
+KO_path=${data_path}/${KO}
+
 
 #################################################
 ############### Quality Control #################
@@ -24,12 +33,14 @@ trimReads -q 20 -m 20 -f ${data_path}/adapters.fasta ${KO_path}.fastq
 ./FastQC/fastqc ${WT_path}.trimmed.fastq
 ./FastQC/fastqc ${KO_path}.trimmed.fastq
 
+
 #################################################
 ################### BWA #########################
 #################################################
 
 python aln_bwa.py -n ${WT_path}.trimmed -i ${data_path}/${index}
 python aln_bwa.py -n ${KO_path}.trimmed -i ${data_path}/${index}
+
 
 #TODO add code for removing dups
 #################################################
@@ -46,8 +57,9 @@ python determine_gapsize.py --ko ${data_path}/sorted-${KO}.sam.bed --wt ${data_p
 best_gap=800
 sh ~/Downloads/SICER_V1.1/SICER/SICER-df-rb.sh ${data_path}/sorted-${KO}.sam.bed ${data_path}/sorted-${WT}_head.sam.bed 200 ${best_gap} 0.1 0.001 > $data_path/SICER_res/
 
+
 #################################################
-############## Get enrichment ###################
+############## Gene enrichment ##################
 #################################################
 
 

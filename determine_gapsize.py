ab3abc377c01a269bf7ac7c0bdf98a5bf517e44d
Gina Turco <gturco88@gmail.com>
2013-08-27 10:05:01 -0700

    working graph code need to fix ggplot part and fixed path for determine gapsize

--- determine_gapsize.py
+++ determine_gapsize.py
@@ -1,4 +1,5 @@
 import commands
+import sys
 
 def move_files(ko,wt,outdir):
     """move island score files to own dir"""
@@ -15,31 +16,32 @@ def move_files(ko,wt,outdir):
             commands.getoutput("mv {0} to {1}/{2}/".format(sfile,outdir,wt))
  
 
-def get_group(ko,wt,window_size):
-""" for many gap sizes run sicer on sample data to plot gaps need to run this script in the dir you want file"""
+def get_group(ko,wt,window_size,outdir):
+    """for many gap sizes run sicer on sample data to plot gaps need to run this script in the dir you want file"""
     for gap in range(0,6):
         gap_size = gap * window_size
         ##TODO is pvalue too high or too low papers recommend otherwise
-        sicer_cmd = "sh /usr/local/bin/SICER_V1.1/SICER/SICER-df-rb.sh {0} {1} {2} {3} 0.1 0.001".format(ko,wt,window_size,gap_size)
+        sicer_cmd = "sh /usr/local/bin/SICER_V1.1/SICER/SICER-df-rb.sh {0} {1} {2} {3} 0.1 0.001 -o {4}".format(ko,wt,window_size,gap_size,outdir)
+        print >>sys.stderr, sicer_cmd
         commands.getoutput(sicer_cmd)
 
-def main(ko,wt,window_size,outdir)
-    get_group(ko,wt,window_size)
-    move_files(ko,wt,outdir)
+def main(ko,wt,outdir,window_size=200):
+    get_group(ko,wt,window_size,outdir)
+    #move_files(ko,wt,outdir)
 
 
 
 if __name__ == "__main__":
     import optparse
     parser = optparse.OptionParser("usage: %prog [options] ")
-    parser.add_option("--ko", dest="ko", help="ko sam.bed file")
-    parser.add_option("--wt", dest="wt", help="wt sam.bed file")
+    parser.add_option("--ko", dest="ko", help="ko file")
+    parser.add_option("--wt", dest="wt", help="wt file")
     parser.add_option("-w", dest="window_size", help="window size", type='int', default=200)
     parser.add_option("-o", dest="outdir", help="location of outdir you want made")
 
     (options, _) = parser.parse_args()
-    if not (options.sample_name and options.index):
-    sys.exit(parser.print_help())
+    if not (options.ko and options.wt and options.outdir):
+        sys.exit(parser.print_help())
 
-    main(options.ko, options.wt,options.window_size)
+    main(options.ko, options.wt,options.outdir,options.window_size)
 
--- graph_gap_size.R
+++ graph_gap_size.R
@@ -2,30 +2,32 @@
 # read.table("testing.txt", "\t", header = FALSE, skip=3)
 
 graph_gap_sizes = function(dir)
-  {
-
+{
+  
   gap_sizes = list()
   islands = list()
-
+  types = list()
+  i = 0
   for (file_name in list.files(dir, full.names=TRUE))
-    {
+  {
     chip_data <- read.table(file_name, sep="\t")
     island_total <- sum(chip_data[4])
+    type <- gap_str <- unlist(strsplit(file_name, "-"))[2]
     gap_str <- unlist(strsplit(file_name, "-"))[4]
     gap_size <- as.numeric(substring(gap_str,2))
-  
-    # append to lists
-    gap_sizes <- c(gap_sizes, gap_size)
-    islands <- c(islands, island_total)
-    }
-  return(gap_size, islands)
-    }
 
-
-# getdata
-gap_size, islands <- graph_gap_sizes(location_dir)
+    
+    # append to lists 
+    gap_sizes[i] = gap_size
+    islands[i] = island_total
+    types[i] = type
+    i = i + 1
+  }
+  data_frame = cbind(types,gap_sizes, islands)
+  return(data_frame)
+}
 
 #plot
 library(ggplot2)
-qplot(gap_size,islands) + geom_smooth()
+qplot(gap_size,islands,data=d) + geom_smooth()
 
